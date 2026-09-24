9c8751940582c9c3ddc7c3f04f0644079da752a7
will <devpedrowill@gmail.com>
2026-09-24 15:54:45 -0300

    Adicao player

---
+++ src/mocks/player.ts
@@ -0,0 +1,12 @@
+import { Player } from "../types/domain";
+
+export const player: Player = {
+    name: "Will",
+    characterClass: "Mago dos Estados",
+    role : "player",
+    level : 4,
+    xp: 430,
+    nextLevelXp : 680,
+    coins : 120,
+    streak: 5, 
+};
