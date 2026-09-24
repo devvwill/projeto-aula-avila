012213d5e5ebd6f4a0192c4b691f88aebd011553
will <devpedrowill@gmail.com>
2026-09-23 22:18:49 -0300

    criacao

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={styles.row}>
+        <Text>Lyra</Text>
+        <Text>Lvl 4</Text>
+      </View>
+
+      <Text>Minha Jornada</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 20,
+    gap: 12,
   },
 });
---
+++ src/theme.js
@@ -0,0 +1,19 @@
+export const colors = {
+    background: "#282a36",
+    surface: "#343746",
+    border: "#44475A",
+    text: "#F8F8F2",
+    muted: "#6272A4",
+    purple:"#BD93F9",
+    green: "#50FA7B",
+    orange: "#FFB86C",
+    red: "#FF5555",
+    bossBackground:"#4C283C",
+};
+
+export const fonts = {
+    regular: "Inter_400Regular",
+    semibold: "Inter_600SemiBold",
+    bold: "Inter_700Bold",
+    extrabolda: "Inter "
+};
